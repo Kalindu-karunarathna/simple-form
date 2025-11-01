c833b9afcc15090c23c92045d2844deb994303d0
Kalindu-Karunarathna <kalindukarunarathne626@gmail.com>
2025-11-01 12:35:26 +0530

    update code to display the user entered details.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+
+
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended : true}));
 
@@ -24,13 +26,19 @@ app.use(myfunc);
 
 app.post("/submit",(req,res)=>{
    console.log(req.body);
-  
+   
+   const name = req.body.name;
+   const email = req.body.email;
+   res.send(`<h3>Form submitted</h3><p>name : ${name}</p><p>email : ${email}</p>`);
+   
 });
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 });
 
+
 app.listen(port,()=>{
     console.log("port is 3000");
 });
+
