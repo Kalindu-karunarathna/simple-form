0a763dfe6ca6ff7a7bc33a67c6bccd9e0dcd9795
Kalindu-Karunarathna <kalindukarunarathne626@gmail.com>
2025-11-01 11:29:54 +0530

    add morgan package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import morgan from "morgan";
 import {dirname} from "path";
 import { fileURLToPath } from "url";
 
@@ -8,6 +9,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended : true}));
 
 
